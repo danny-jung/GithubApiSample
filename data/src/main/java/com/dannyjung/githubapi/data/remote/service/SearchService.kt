0137e7425cefdf57f62f59063877ce40de4d1a3c
danny-jung <iiarbutusi@gmail.com>
2022-02-21 21:46:12 +0900

    SearchService 추가

---
+++ data/src/main/java/com/dannyjung/githubapi/data/remote/service/SearchService.kt
@@ -0,0 +1,17 @@
+package com.dannyjung.githubapi.data.remote.service
+
+import com.dannyjung.githubapi.data.model.SearchRepoResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface SearchService {
+
+    @GET("/search/repositories")
+    suspend fun searchRepositories(
+        @Header("Authorization") token: String?,
+        @Query("q") query: String,
+        @Query("per_page") pageSize: Int,
+        @Query("page") page: Int,
+    ): SearchRepoResponse
+}
