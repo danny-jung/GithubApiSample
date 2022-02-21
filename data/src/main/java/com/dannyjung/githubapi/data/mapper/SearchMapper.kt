987104d51bc70be65e09ff483e13aea9066e8f0a
danny-jung <iiarbutusi@gmail.com>
2022-02-21 21:44:47 +0900

    SearchMapper 추가

---
+++ data/src/main/java/com/dannyjung/githubapi/data/mapper/SearchMapper.kt
@@ -0,0 +1,28 @@
+package com.dannyjung.githubapi.data.mapper
+
+import com.dannyjung.githubapi.data.model.SearchRepoResponse
+import com.dannyjung.githubapi.domain.model.Item
+import com.dannyjung.githubapi.domain.model.Owner
+import com.dannyjung.githubapi.domain.model.SearchRepo
+
+object SearchMapper {
+
+    fun mapperToSearchRepo(searchRepoResponse: SearchRepoResponse): SearchRepo =
+        SearchRepo(
+            totalCount = searchRepoResponse.totalCount,
+            items = searchRepoResponse.items.map { item ->
+                Item(
+                    id = item.id,
+                    name = item.name,
+                    description = item.description,
+                    owner = Owner(
+                        id = item.owner.id,
+                        name = item.owner.login,
+                        avatarUrl = item.owner.avatarUrl
+                    ),
+                    stargazersCount = item.stargazersCount,
+                    watchersCount = item.watchersCount
+                )
+            }
+        )
+}
