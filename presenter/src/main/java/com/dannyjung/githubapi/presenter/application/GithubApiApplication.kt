d5c27e196fa8d74a8a0e29d8d7976ba7eb85955d
danny-jung <iiarbutusi@gmail.com>
2022-02-21 21:40:37 +0900

    Stetho 초기화 로직 추가

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/application/GithubApiApplication.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/application/GithubApiApplication.kt
@@ -1,7 +1,19 @@
 package com.dannyjung.githubapi.presenter.application
 
 import android.app.Application
+import android.webkit.WebView
+import com.dannyjung.githubapi.presenter.BuildConfig
+import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GithubApiApplication : Application()
+class GithubApiApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
+    }
+}
