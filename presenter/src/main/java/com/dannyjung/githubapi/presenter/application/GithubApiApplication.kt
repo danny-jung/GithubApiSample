975f4646c82a6b099a78f94edc861a4c27ab2855
danny-jung <iiarbutusi@gmail.com>
2022-02-21 16:32:55 +0900

    Application 적용

---
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/application/GithubApiApplication.kt
@@ -0,0 +1,7 @@
+package com.dannyjung.githubapi.presenter.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GithubApiApplication : Application()
