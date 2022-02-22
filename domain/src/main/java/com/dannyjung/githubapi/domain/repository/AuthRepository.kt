8e68787b841e3e28e177286ac162ec14151a1f29
danny-jung <iiarbutusi@gmail.com>
2022-02-22 16:13:10 +0900

    AuthRepository 추가

---
+++ domain/src/main/java/com/dannyjung/githubapi/domain/repository/AuthRepository.kt
@@ -0,0 +1,15 @@
+package com.dannyjung.githubapi.domain.repository
+
+import com.dannyjung.githubapi.domain.model.AccessToken
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepository {
+
+    val stream: Flow<String?>
+
+    val accessToken: String?
+
+    suspend fun requestAccessToken(code: String): AccessToken
+
+    fun clear()
+}
