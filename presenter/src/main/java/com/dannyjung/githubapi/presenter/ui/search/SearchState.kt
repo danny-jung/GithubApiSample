7b20c5e97a60462431a50df8b3f43a7c4eae6797
danny-jung <iiarbutusi@gmail.com>
2022-02-22 19:44:35 +0900

    SearchState 데이터 구성

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/search/SearchState.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/search/SearchState.kt
@@ -3,7 +3,11 @@ package com.dannyjung.githubapi.presenter.ui.search
 import com.airbnb.mvrx.Async
 import com.airbnb.mvrx.MavericksState
 import com.airbnb.mvrx.Uninitialized
+import com.dannyjung.githubapi.domain.model.SearchRepo
 
 data class SearchState(
-    val initialAsync: Async<Unit> = Uninitialized
+    val searchRepoAsync: Async<SearchRepo> = Uninitialized,
+    val keyword: String? = null,
+    val searchRepo: SearchRepo? = null,
+    val page: Int = 1
 ) : MavericksState
