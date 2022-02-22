3a5da1b8155039bd2fcf244ee82072d4b4f2fac8
danny-jung <iiarbutusi@gmail.com>
2022-02-22 16:47:45 +0900

    Authorize 콜백 로직 추가

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/MainActivity.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.dannyjung.githubapi.presenter
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.airbnb.mvrx.MavericksView
+import com.airbnb.mvrx.viewModel
 import com.dannyjung.githubapi.presenter.databinding.ActivityMainBinding
+import com.dannyjung.githubapi.presenter.ui.login.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MavericksView {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val loginViewModel: LoginViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +26,20 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.data?.getQueryParameter(CODE)?.let { code ->
+            loginViewModel.requestAccessToken(code)
+        }
+    }
+
+    override fun invalidate() = Unit
+
+    companion object {
+
+        private const val CODE = "code"
     }
 }
