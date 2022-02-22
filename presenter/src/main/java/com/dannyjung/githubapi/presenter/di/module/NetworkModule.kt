6283fb983042b6fc773795266e82160090815454
danny-jung <iiarbutusi@gmail.com>
2022-02-22 16:27:50 +0900

    Gson setLenient 설정

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/di/module/NetworkModule.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/di/module/NetworkModule.kt
@@ -6,6 +6,8 @@ import com.dannyjung.githubapi.presenter.BuildConfig
 import com.dannyjung.githubapi.presenter.di.qualifiers.AuthRetrofit
 import com.dannyjung.githubapi.presenter.di.qualifiers.GitHubRetrofit
 import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,6 +16,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -42,21 +45,26 @@ object NetworkModule {
 
     @AuthRetrofit
     @Provides
-    fun provideAuthRetrofit(client: OkHttpClient): Retrofit =
+    fun provideAuthRetrofit(client: OkHttpClient, gson: Gson): Retrofit =
         Retrofit.Builder()
             .baseUrl(BuildConfig.AUTH_BASE_URL)
             .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
     @GitHubRetrofit
     @Provides
-    fun provideGithubRetrofit(client: OkHttpClient): Retrofit =
+    fun provideGithubRetrofit(client: OkHttpClient, gson: Gson): Retrofit =
         Retrofit.Builder()
             .baseUrl(BuildConfig.GITHUB_BASE_URL)
             .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson =
+        GsonBuilder().setLenient().create()
 
     @Provides
     fun provideAuthService(
