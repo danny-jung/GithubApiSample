59c3fc5044853b320d898bbc1f4be2f2efb29897
danny-jung <iiarbutusi@gmail.com>
2022-02-22 16:25:20 +0900

    UseCaseModule 수정

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/di/module/UseCaseModule.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/di/module/UseCaseModule.kt
@@ -1,6 +1,10 @@
 package com.dannyjung.githubapi.presenter.di.module
 
+import com.dannyjung.githubapi.domain.repository.AuthRepository
 import com.dannyjung.githubapi.domain.repository.SearchRepository
+import com.dannyjung.githubapi.domain.usecase.auth.AccessTokenStreamUseCase
+import com.dannyjung.githubapi.domain.usecase.auth.ClearAccessTokenUseCase
+import com.dannyjung.githubapi.domain.usecase.auth.RequestAccessTokenUseCase
 import com.dannyjung.githubapi.domain.usecase.search.SearchRepositoriesUseCase
 import dagger.Module
 import dagger.Provides
@@ -15,4 +19,19 @@ object UseCaseModule {
     fun provideSearchRepositoriesUseCase(
         searchRepository: SearchRepository
     ) = SearchRepositoriesUseCase(searchRepository)
+
+    @Provides
+    fun provideRequestAccessTokenUseCase(
+        authRepository: AuthRepository
+    ) = RequestAccessTokenUseCase(authRepository)
+
+    @Provides
+    fun provideClearAccessTokenUseCase(
+        authRepository: AuthRepository
+    ) = ClearAccessTokenUseCase(authRepository)
+
+    @Provides
+    fun provideAccessTokenStreamUseCase(
+        authRepository: AuthRepository
+    ) = AccessTokenStreamUseCase(authRepository)
 }
