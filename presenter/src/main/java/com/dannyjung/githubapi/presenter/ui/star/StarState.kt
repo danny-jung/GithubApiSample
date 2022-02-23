d8a2df72f9f8ef3c0bd4ca9b797a46b54f0365a7
danny-jung <iiarbutusi@gmail.com>
2022-02-23 13:53:53 +0900

    StarViewModel 구현

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/star/StarState.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/star/StarState.kt
@@ -3,7 +3,12 @@ package com.dannyjung.githubapi.presenter.ui.star
 import com.airbnb.mvrx.Async
 import com.airbnb.mvrx.MavericksState
 import com.airbnb.mvrx.Uninitialized
+import com.dannyjung.githubapi.domain.model.StarredRepoItem
 
 data class StarState(
-    val initialAsync: Async<Unit> = Uninitialized
+    val getReposAsync: Async<Unit> = Uninitialized,
+    val addRepoAsync: Async<Unit> = Uninitialized,
+    val deleteRepoAsync: Async<Unit> = Uninitialized,
+    val repos: List<StarredRepoItem>? = null,
+    val allRepoIds: Set<Long>? = null
 ) : MavericksState
--- presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/star/StarViewModel.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/star/StarViewModel.kt
@@ -1,16 +1,121 @@
 package com.dannyjung.githubapi.presenter.ui.star
 
-import com.airbnb.mvrx.MavericksViewModel
-import com.airbnb.mvrx.MavericksViewModelFactory
+import com.airbnb.mvrx.*
 import com.airbnb.mvrx.hilt.AssistedViewModelFactory
 import com.airbnb.mvrx.hilt.hiltMavericksViewModelFactory
+import com.dannyjung.githubapi.domain.model.StarredRepoItem
+import com.dannyjung.githubapi.domain.usecase.starredrepo.AddStarredRepoUseCase
+import com.dannyjung.githubapi.domain.usecase.starredrepo.DeleteStarredRepoUseCase
+import com.dannyjung.githubapi.domain.usecase.starredrepo.GetAllStarredRepoIdsUseCase
+import com.dannyjung.githubapi.domain.usecase.starredrepo.GetStarredReposUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class StarViewModel @AssistedInject constructor(
-    @Assisted initialState: StarState
+    @Assisted initialState: StarState,
+    private val getStarredReposUseCase: GetStarredReposUseCase,
+    private val getAllStarredRepoIdsUseCase: GetAllStarredRepoIdsUseCase,
+    private val addStarredRepoUseCase: AddStarredRepoUseCase,
+    private val deleteStarredRepoUseCase: DeleteStarredRepoUseCase
 ) : MavericksViewModel<StarState>(initialState) {
+
+    fun invalidate() = viewModelScope.launch {
+        val state = awaitState()
+
+        if (state.getReposAsync is Loading) return@launch
+
+        setState { copy(getReposAsync = Loading()) }
+
+        val reposDeferred = async { getStarredReposUseCase(offset = state.repos?.size ?: 0) }
+        val repoIdsDeferred = async { getAllStarredRepoIdsUseCase() }
+
+        val reposAsync = reposDeferred.await()
+        val repoIdsAsync = repoIdsDeferred.await()
+
+        setState {
+            copy(
+                getReposAsync = if (reposAsync is Fail || repoIdsAsync is Fail) {
+                    Fail(
+                        (repoIdsAsync as? Fail)?.error
+                            ?: (repoIdsAsync as? Fail)?.error
+                            ?: IOException("unknown error")
+                    )
+                } else {
+                    Success(Unit)
+                },
+                repos = reposAsync(),
+                allRepoIds = repoIdsAsync()?.toSet()
+            )
+        }
+    }
+
+    fun addRepo(repoItem: StarredRepoItem) = viewModelScope.launch {
+        val state = awaitState()
+
+        if (state.addRepoAsync is Loading) return@launch
+
+        setState {
+            copy(
+                addRepoAsync = Loading(),
+                repos = repos?.plus(repoItem),
+                allRepoIds = allRepoIds?.plus(repoItem.id)
+            )
+        }
+
+        val async = addStarredRepoUseCase(repoItem)
+
+        setState {
+            copy(
+                addRepoAsync = async,
+                repos = if (async is Fail) {
+                    repos?.minus(repoItem)
+                } else {
+                    repos
+                },
+                allRepoIds = if (async is Fail) {
+                    allRepoIds?.minus(repoItem.id)
+                } else {
+                    allRepoIds
+                }
+            )
+        }
+    }
+
+    fun deleteRepo(repoItem: StarredRepoItem) = viewModelScope.launch {
+        val state = awaitState()
+
+        if (state.deleteRepoAsync is Loading) return@launch
+
+        setState {
+            copy(
+                deleteRepoAsync = Loading(),
+                repos = repos?.minus(repoItem),
+                allRepoIds = allRepoIds?.minus(repoItem.id)
+            )
+        }
+
+        val async = deleteStarredRepoUseCase(repoItem)
+
+        setState {
+            copy(
+                deleteRepoAsync = async,
+                repos = if (async is Fail) {
+                    repos?.plus(repoItem)
+                } else {
+                    repos
+                },
+                allRepoIds = if (async is Fail) {
+                    allRepoIds?.plus(repoItem.id)
+                } else {
+                    allRepoIds
+                }
+            )
+        }
+    }
 
     @AssistedFactory
     interface Factory : AssistedViewModelFactory<StarViewModel, StarState> {
