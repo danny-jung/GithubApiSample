e76078cb0344d52922ba90ab120e74667c88e8c0
danny-jung <iiarbutusi@gmail.com>
2022-02-23 10:42:33 +0900

    마이페이지 데이터 요청 및 클리어 로직 추가

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/mypage/MyPageState.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/mypage/MyPageState.kt
@@ -3,7 +3,11 @@ package com.dannyjung.githubapi.presenter.ui.mypage
 import com.airbnb.mvrx.Async
 import com.airbnb.mvrx.MavericksState
 import com.airbnb.mvrx.Uninitialized
+import com.dannyjung.githubapi.domain.model.RepoItem
+import com.dannyjung.githubapi.domain.model.User
 
 data class MyPageState(
-    val initialAsync: Async<Unit> = Uninitialized
+    val initialAsync: Async<Unit> = Uninitialized,
+    val user: User? = null,
+    val userRepos: List<RepoItem>? = null
 ) : MavericksState
--- presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/mypage/MyPageViewModel.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/mypage/MyPageViewModel.kt
@@ -1,16 +1,53 @@
 package com.dannyjung.githubapi.presenter.ui.mypage
 
-import com.airbnb.mvrx.MavericksViewModel
-import com.airbnb.mvrx.MavericksViewModelFactory
+import com.airbnb.mvrx.*
 import com.airbnb.mvrx.hilt.AssistedViewModelFactory
 import com.airbnb.mvrx.hilt.hiltMavericksViewModelFactory
+import com.dannyjung.githubapi.domain.usecase.user.GetUserReposUseCase
+import com.dannyjung.githubapi.domain.usecase.user.GetUserUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.launch
 
 class MyPageViewModel @AssistedInject constructor(
-    @Assisted initialState: MyPageState
+    @Assisted initialState: MyPageState,
+    private val getUserUseCase: GetUserUseCase,
+    private val getUserReposUseCase: GetUserReposUseCase
 ) : MavericksViewModel<MyPageState>(initialState) {
+
+    fun initialize() = viewModelScope.launch {
+        val state = awaitState()
+
+        if (state.initialAsync is Loading) return@launch
+
+        setState { copy(initialAsync = Loading()) }
+
+        val user = getUserUseCase()
+        val userRepos = user()?.let {
+            getUserReposUseCase(
+                userName = it.name,
+                page = 1
+            )
+        }
+
+        setState {
+            copy(
+                initialAsync = Success(Unit),
+                user = user(),
+                userRepos = userRepos?.invoke()
+            )
+        }
+    }
+
+    fun clear() =
+        setState {
+            copy(
+                initialAsync = Uninitialized,
+                user = null,
+                userRepos = null
+            )
+        }
 
     @AssistedFactory
     interface Factory : AssistedViewModelFactory<MyPageViewModel, MyPageState> {
