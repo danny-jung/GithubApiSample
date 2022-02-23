a9bf35d39ef3d7279c673a79342848f858ad3c37
danny-jung <iiarbutusi@gmail.com>
2022-02-23 10:43:15 +0900

    MyPage EpoxyRecyclerView 추가

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/mypage/MyPageFragment.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/mypage/MyPageFragment.kt
@@ -5,12 +5,19 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.airbnb.mvrx.activityViewModel
 import com.airbnb.mvrx.fragmentViewModel
+import com.dannyjung.githubapi.presenter.R
 import com.dannyjung.githubapi.presenter.databinding.FragmentMyPageBinding
 import com.dannyjung.githubapi.presenter.ui.base.BaseFragment
+import com.dannyjung.githubapi.presenter.ui.component.profile
+import com.dannyjung.githubapi.presenter.ui.component.repoListItem
+import com.dannyjung.githubapi.presenter.ui.component.space
+import com.dannyjung.githubapi.presenter.ui.epoxy.simpleEpoxyController
 import com.dannyjung.githubapi.presenter.ui.login.LoginState
 import com.dannyjung.githubapi.presenter.ui.login.LoginViewModel
+import com.dannyjung.githubapi.presenter.ui.utils.dpToPx
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +26,8 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding
     private val myPageViewModel: MyPageViewModel by fragmentViewModel()
     private val loginViewModel: LoginViewModel by activityViewModel()
 
+    private val epoxyController by lazy { createEpoxyController() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -26,6 +35,16 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding
 
         loginViewModel.onEach(LoginState::accessToken) { accessToken ->
             binding.loginButton.isVisible = accessToken == null
+            binding.epoxyRecyclerView.isVisible = accessToken != null
+
+            if (accessToken != null) {
+                myPageViewModel.initialize()
+            }
+        }
+
+        binding.epoxyRecyclerView.run {
+            layoutManager = LinearLayoutManager(requireContext())
+            setController(epoxyController)
         }
     }
 
@@ -39,5 +58,50 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding
         }
     }
 
-    override fun invalidate() = Unit
+    override fun invalidate() = epoxyController.requestModelBuild()
+
+    private fun createEpoxyController() = simpleEpoxyController(myPageViewModel) { myPageState ->
+        myPageState.user?.let { user ->
+            profile {
+                id("profile")
+                ownerAvatarUrl(user.avatarUrl)
+                ownerName(user.name)
+                company(user.company)
+                location(user.location)
+                url(user.url)
+                onLogoutButtonClick {
+                    loginViewModel.clearAccessToken()
+                    myPageViewModel.clear()
+                }
+            }
+
+            space {
+                id("space")
+                backgroundColor(R.color.gray_10)
+                height(requireContext().dpToPx(8))
+            }
+        }
+
+        myPageState.userRepos?.forEach { item ->
+            repoListItem {
+                id("repo_list_item", item.id)
+                repoId(item.id)
+                repoName(item.name)
+                description(item.description)
+                stargazersCount(item.stargazersCount)
+                watchersCount(item.watchersCount)
+                ownerAvatarUrl(item.owner.avatarUrl)
+                ownerName(item.owner.name)
+                onStarClick { repoId ->
+                    // TODO
+                }
+            }
+
+            space {
+                id("space", item.id)
+                backgroundColor(R.color.gray_10)
+                height(requireContext().dpToPx(8))
+            }
+        }
+    }
 }
