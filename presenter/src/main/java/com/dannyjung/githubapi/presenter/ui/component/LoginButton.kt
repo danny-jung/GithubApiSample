aff6c4144fd8d72fe2d3eedbb4c1b1495fb7ef6e
danny-jung <iiarbutusi@gmail.com>
2022-02-23 10:40:58 +0900

    LoginButton 디자인 변경

--- presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/component/LoginButton.kt
+++ presenter/src/main/java/com/dannyjung/githubapi/presenter/ui/component/LoginButton.kt
@@ -5,9 +5,9 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.Gravity
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import com.dannyjung.githubapi.presenter.R
 import com.dannyjung.githubapi.presenter.ui.utils.dpToPx
-import com.dannyjung.githubapi.presenter.ui.utils.getResourceIdWithAttr
 
 class LoginButton @JvmOverloads constructor(
     context: Context,
@@ -18,15 +18,9 @@ class LoginButton @JvmOverloads constructor(
     init {
         gravity = Gravity.CENTER
         text = "로그인"
-        setTextColor(context.getResourceIdWithAttr(R.attr.colorOnSecondary))
-        setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
-        setBackgroundResource(R.drawable.shape_rounded_rect_4_color_secondary)
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(
-            MeasureSpec.makeMeasureSpec(context.dpToPx(150), MeasureSpec.EXACTLY),
-            MeasureSpec.makeMeasureSpec(context.dpToPx(50), MeasureSpec.EXACTLY)
-        )
+        setPadding(context.dpToPx(34), context.dpToPx(16), context.dpToPx(34), context.dpToPx(16))
+        setTextColor(ContextCompat.getColor(context, R.color.white))
+        setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+        setBackgroundResource(R.drawable.shape_rounded_rect_4_color_primary_variant)
     }
 }
